a2d08b434ea3069b6860e0a24ea41bec2090ecd6
Azael Acin <goodtester24@gmail.com>
2025-02-12 20:16:30 +0100

    Java creado correctamente

---
+++ sources/AzaelAcin/src/main/java/com/mycompany/azaelacin/AzaelAcin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.azaelacin;
+
+/**
+ *
+ * @author GoodTester24
+ */
+public class AzaelAcin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
